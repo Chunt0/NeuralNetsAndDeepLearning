f0a10d64fc4eb9edce65fcff5fa25aaf7f79e57d
Chunt0 <chunt.mail@gmail.com>
2022-04-09 12:36:10 -0700

    Update driver

--- MNIST/driver.py
+++ MNIST/driver.py
@@ -1,16 +1,16 @@
 import random
 from network import Network
-from MNIST_crit_funcs import testInput, trainInput
+from MNIST_crit_funcs import fileInput
 import numpy as np
 
 size = (784,15,15,10)
 net = Network(size)
 
-file1 = "/home/chunt/VScode/Python/NeuralNetsAndDeepLearning/MNIST/MNISTtraintest/mnist_test.csv"
-test_image, test_label = trainInput(file1,10000)
+test_file = "/home/chunt/VScode/Data/MNIST/mnist_test.csv"
+test_image, test_label = fileInput(test_file,10000)
 
-file2 = "/home/chunt/VScode/Python/NeuralNetsAndDeepLearning/MNIST/MNISTtraintest/mnist_train.csv"
-train_image, train_label = trainInput(file2,60000)
+train_file = "/home/chunt/VScode/Data/MNIST/mnist_train.csv"
+train_image, train_label = fileInput(train_file,60000)
 
 
 print(f"test_label[0]: {test_image.T.shape}")
