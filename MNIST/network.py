bd041620a82e615257f4d633e5009bcd33bd6239
Chunt0 <chunt.mail@gmail.com>
2022-04-09 13:39:57 -0700

    Updated feedForward methods

--- MNIST/network.py
+++ MNIST/network.py
@@ -18,9 +18,10 @@ class Network:
         self.weights = [np.random.randn(x,y) for x,y in zip(sizes[1:], sizes[:-1])]  # (from index 1 until the end, from index 0 to one before the end)
         self.biases = [np.random.randn(y,1) for y in sizes[1:]] # (from index 1 until the end, always 1)
 
-    def feedForward(self, a):
-        """Return the output of the network if "a" is input"""
-        
+    def feedForwardRelu(self, a):
+        """Return the output of the network if "a" is input using ReLU and softmax as 
+        activation functions - ReLU on all layers except the final which uses a softmax
+        function to return values between 0 - 1 """
         # Both the weight and bias matrices have the same amount of layers (in this instance it is two, in each layer the matrices have the same rows but different collumn amount)
         last = len(self.biases)-1
         count = 0
@@ -32,8 +33,18 @@ class Network:
                 a = softmax(np.dot(w,a)+b)
         return a
 
+    def feedForwardSigmoid(self, a):
+        """Returns the output of the network if "a" is the input using the Sigmoid
+        function as the activation function on all layers"""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w,a)+b)
+            return a
+
     def stochasticGradientDescent(self, training_data, epochs, mini_batch_size, eta, test_data=None):
         """Finding Global Minima -> in this case minimizing the cost function. Stochastic Gradient Descent is designed such that
         we can better avoid getting caught in local minima to find the global minima."""
         if test_data: n_test = len(test_data)
+        pass
+
+    def selectionMenu(self):
         pass
